76f9a9f1e70781979b5d3dced86e6315701ff038
Guilagarto <lagartoorei@gmail.com>
2022-04-06 20:30:36 -0400

    Servidor Funcionando

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname,'viewa/index.html')))
+
+app.listen(port, () => console.info(`Servidor rodando na porta ${port}`))
